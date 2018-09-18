f2fb831c9678ead946220fa879bfe48b914ae283
Concross <cacrossley22@gmail.com>
2018-09-18 08:08:09 -0700

    code review

--- server/__tests__/src/lib/api.test.js
+++ server/__tests__/src/lib/api.test.js
@@ -1,8 +1,17 @@
 'use strict';
 
 let router = require('../../../src/lib/router');
+let app = require('../../../src/app');
+let request = require('supertest');
 
-describe('API tests', () => {
-
-
+describe('POST /api/v1/notes', () => {
+  it('should return a 200 response', (done) => {
+    let content = JSON.stringify({
+      my: 'object',
+    });
+    request('localhost:3000')
+      .post('/api/v1/notes')
+      .send(content)
+      .expect(200, content, done);
+  });
 });
--- server/src/api/api.js
+++ server/src/api/api.js
@@ -1,3 +1,24 @@
 'use strict';
 
 const router = require('../lib/router.js');
+
+let basePath = '/api/v1';
+
+router.post(`${basePath}/notes`, (req, res) => {
+  // do stuff
+  res.status = 200;
+  res.write(req.body);
+  res.end();
+});
+
+router.put('/', (req, res) => {
+  // do stuff
+});
+
+router.get('/', (req, res) => {
+  // do stuff
+});
+
+router.delete('/', (req, res) => {
+  // do stuff
+});
