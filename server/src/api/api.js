c0d872ca7eeca9f44bc654f7e67ed7ed116ab304
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-18 18:23:07 -0700

    added code for api

--- server/src/api/api.js
+++ server/src/api/api.js
@@ -14,3 +14,31 @@ router.get('/', (req,res) => {
 
 //router(get/)callback
 
+const router = require('../lib/router.js');
+const notes = require('../models/notes.js');
+const api = module.exports = {};
+
+let sendJSON = (res, data) => {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(req.body));
+  res.end();
+};
+
+let serverError = (res, err) => {
+  let error = { error: err };
+  res.statusCode = 500;
+  res.statusMessage = 'Server Error';
+  res.setHeader('Content-Type', 'appliation/json');
+  res.write(JSON.stringify(error));
+  res.end();
+};
+
+
+router.post('/api/v1/notes', (req, res) => {
+  let record = new Notes(req.body.title, req.body.content);
+  record.save()
+    .then(data => sendJSON(req, data))
+    .catch(err => serverError(res, err));
+});
